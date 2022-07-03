53da4450482c8677332b9a8dbd5a7201b4baa4f2
Vasu Bhalodi <88498558+vasubhalodi07@users.noreply.github.com>
2022-07-03 15:19:29 +0530

    Add files via upload

---
+++ JavaScript/Basic/switch.js
@@ -0,0 +1,12 @@
+var a = 2
+switch (a) {
+    case 1:
+        console.log("1");
+    break;
+    case 2:
+        console.log("2");
+    break;
+    default:
+        console.log(a);
+    break;
+}
